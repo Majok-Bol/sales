873146d55d7ecc466a048e3c0401a46a25d27a7e
Majok-Bol <bolmajok001@gmail.com>
2026-02-06 13:52:02 +0300

    installed jupyter

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print('Hello world')
+import pandas as pd 
+import numpy as np 
+#check version of pandas
+print("Pandas version: ",pd.__version__)
+#check versio of numpy
+print("Numpy version: ",np.__version__)
+#read csv data
+sales=pd.read_csv("sales_data.csv")
+# print(sales)
