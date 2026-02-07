37731e630fae6e571ec29a48ada9dc018b45ac9c
Majok-Bol <bolmajok001@gmail.com>
2026-02-07 14:44:46 +0300

    saved sales summary percentage as png

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import pandas as pd 
 import numpy as np 
+import matplotlib
+matplotlib.use("TKAgg")
+import matplotlib.pyplot as plt
 #check version of pandas
 print("Pandas version: ",pd.__version__)
 #check versio of numpy
 print("Numpy version: ",np.__version__)
+print("Matplotlib version: ",matplotlib.__version__)
+print("Matplotlib backend: ",matplotlib.get_backend())
 #read csv data
 sales=pd.read_csv("sales_data.csv")
 # print(sales)
+sales.head()
